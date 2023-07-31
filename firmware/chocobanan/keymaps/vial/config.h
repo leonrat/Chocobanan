f530d2255a469f81e1aca25089c7693705c815bf
leonrat <leon.ratinckx@skynet.be>
2023-07-31 14:29:30 +0200

    changes to make sure repo up to date for new pc

---
+++ firmware/chocobanan/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 Léon Ratinckx (@leonrat)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x4B, 0x1C, 0x43, 0x9D, 0x2E, 0xB9, 0x28, 0xFB}
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
---
+++ firmware/chocobanan/keymaps/vial/keymap.c
@@ -0,0 +1,19 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT (
+        KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,
+        MO(1),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,
+        KC_LCTL,   KC_LGUI,   KC_LALT,   KC_SPC,   KC_ENT,   RGB_TOG
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+};
+#endif
